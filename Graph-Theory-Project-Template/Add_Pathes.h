1801b1b1fa9fcdd48eaa4ffd63c09272e5a216c0
LeoNCKU <50820380+LeoNCKU@users.noreply.github.com>
2019-06-11 16:41:51 +0800

    Everything is done

---
+++ Graph-Theory-Project-Template/Add_Pathes.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+class OBJ1
+{
+public:
+	string head;
+	string tail;
+	int  flowval;
+};
---
+++ Graph-Theory-Project-Template/Positive_Negative.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+class OBJ
+{
+public:
+	string Positive;
+	string Negative;
+	int  Additional_Paths;
+};
